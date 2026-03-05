b55e499da0af978918cc9c07b392b3276f78e5f0
MoscowDev <mosesogomegbunam@gmail.com>
2026-03-05 21:40:57 +0100

    new commit

--- src/main/java/com/Moscow/RegisterationApp/data/models/User.java
+++ src/main/java/com/Moscow/RegisterationApp/data/models/User.java
@@ -1,15 +1,20 @@
 package com.Moscow.RegisterationApp.data.models;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document(collection = "users")
 public class User {
+    @Id
+    private String id;
+
     private String username;
     private String password;
     private String email;
     private String firstName;
     private String lastName;
-    private String message;
 
 
 }
